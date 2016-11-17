9ebb33641f0eb1778b6cc038f17182b0fa41e1bc
pengshiqi <617225573@qq.com>
2016-11-18 00:09:03 +0800

    update UI

--- Version2.2/README.md
+++ Version2.2/README.md
@@ -1,12 +1,12 @@
 # BlackWhiteChess
 
-A simple but funny game implemented by python in autumn 2016.
+A simple but funny game implemented by python and C++ in autumn 2016.
 
 Using Minimax Algorithm and Alpha-Beta Pruning.
 
-Having different modes.(Human VS Human and Human VS AI)
+Having different modes(Human VS Human and Human VS AI) and different levels.
 
-Having different levels.
+----
 
 Run othello.py to play the game!
 
--- Version2.2/othello.py
+++ Version2.2/othello.py
@@ -4,7 +4,6 @@
 # playerType: 0 -> Human VS Human, 1 -> Human VS AI
 # difficultLevel: 0 -> easyModeAI, 1 -> mediumModeAI, 2 -> hardModeAI
 
-import copy
 import logging
 from time import sleep
 
@@ -12,11 +11,12 @@ import easyModeAI.calculate
 import mediumModeAI.calculate
 import hardModeAI.calculate
 from graphics import *
-from canvas import Button, Cell, Canvas
+from canvas import Button, Canvas
 
 __author__ = 'patrick_psq'
 
 logging.basicConfig(level=logging.INFO)
+
 
 # 刷新双方棋子信息
 def refresh():
@@ -37,22 +37,44 @@ if __name__ == '__main__':
     t2.setSize(20)
     t2.draw(win)
 
-    t3 = Text(Point(300, 350), "Human VS Human : ")
+    t3 = Text(Point(250, 350), "Human VS Human : ")
     t3.setSize(15)
     t3.draw(win)
 
-    t4 = Text(Point(300, 450), "Human VS AI : ")
+    t4 = Text(Point(250, 450), "Human VS AI : ")
     t4.setSize(15)
     t4.draw(win)
 
-    b_start = Button(win, Point(450, 350), 100, 60, "Start")  # start button
+    text_row = []
+    text_column = []
+    for ch in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+        text_ch = Text(Point((ord(ch) - ord('a')) * 60 + 90, 40), ch)
+        text_ch.setSize(20)
+        text_ch.setOutline('dark blue')
+        text_row.append(text_ch)
+
+    for i in range(1, 9):
+        text_num = Text(Point(40, i * 60 + 30), str(i))
+        text_num.setSize(20)
+        text_num.setOutline('dark blue')
+        text_column.append(text_num)
+
+    t_skip_white = Text(Point(700, 300), "White SKIP a turn!")
+    t_skip_white.setSize(20)
+    t_skip_white.setOutline('white')
+
+    t_skip_black = Text(Point(700, 300), "Black SKIP a turn!")
+    t_skip_black.setSize(20)
+    t_skip_black.setOutline('black')
+
+    b_start = Button(win, Point(400, 350), 100, 60, "Start")  # start button
     b_start.activate()
 
-    b_start_easy = Button(win, Point(450, 450), 100, 60, "Start Easy Mode")
+    b_start_easy = Button(win, Point(400, 450), 100, 60, "Start Easy Mode")
     b_start_easy.activate()
-    b_start_medium = Button(win, Point(580, 450), 100, 60, "Start Medium Mode")
+    b_start_medium = Button(win, Point(530, 450), 100, 60, "Start Medium Mode")
     b_start_medium.activate()
-    b_start_hard = Button(win, Point(710, 450), 100, 60, "Start Hard Mode")
+    b_start_hard = Button(win, Point(660, 450), 100, 60, "Start Hard Mode")
     b_start_hard.activate()
 
     b_qt = Button(win, Point(750, 575), 100, 50, "Quit")  # quit button
@@ -80,7 +102,7 @@ if __name__ == '__main__':
 
     b_re = Button(win, Point(700, 50), 190, 90, "Restart")
     b_re.clear()
-    b_undo = Button(win, Point(700, 300), 100, 60, "Undo")
+    b_undo = Button(win, Point(700, 100), 100, 60, "Undo")
     b_undo.clear()
 
     while 1:
@@ -104,6 +126,11 @@ if __name__ == '__main__':
             t2.undraw()  # Un-draw game name and author name
             t3.undraw()
             t4.undraw()
+
+            for t in text_row:
+                t.draw(win)
+            for t in text_column:
+                t.draw(win)
 
             b_start.clear()  # clear start button
             b_start_easy.clear()
@@ -129,24 +156,15 @@ if __name__ == '__main__':
                         [0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0]
                         ]
-            lastMove = [[0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 1, 2, 0, 0, 0],
-                        [0, 0, 0, 2, 1, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0]
-                        ]
 
             while 1:
                 # 黑子是否可以落子
-                whether_black = canvas.check_black()
-                whether_clicked = False
-                if whether_black:
+                blackHasValidMove = canvas.check_black()
+                blackClicked = False
+                if blackHasValidMove:
                     p2 = win.getMouse()
 
-                    whether_clicked = False  # 黑子方是否落子
+                    blackClicked = False  # 黑子方是否落子
                     if b_qt.clicked(p2):  # 如果点击quit按钮
                         break
                     if b_undo.clicked(p2):  # 如果点击悔棋
@@ -156,6 +174,11 @@ if __name__ == '__main__':
                     for i in range(8):
                         for j in range(8):
                             if canvas.board[i][j].clicked(p2) and canvas.check(i, j, 2):
+                                # update undoList
+                                for x in xrange(8):
+                                    for y in xrange(8):
+                                        undoList[x][y] = canvas.board[x][y].color
+
                                 canvas.click(i, j, 2)
                                 logging.info('BLACK places at (%s, %s)' % (i, j))
                                 canvas.piece_num[2] += 1  # 更新棋盘上两种颜色的棋子数
@@ -166,28 +189,31 @@ if __name__ == '__main__':
 
                                 canvas.total_num += 1
                                 refresh()
-                                whether_clicked = True  # 标记为已经落子
+                                blackClicked = True  # 标记为已经落子
 
-                if not whether_black:
+                if not blackHasValidMove:
                     # 黑色不能落子
+                    t_skip_black.draw(win)
+                    sleep(1)
+                    t_skip_black.undraw()
+
                     black_turn.undraw()
                     white_turn.draw(win)
-                    canvas.total_num += 1
-                    whether_clicked = True
+                    # canvas.total_num += 1
+                    blackClicked = True
 
-                if whether_clicked:
+                if blackClicked:
                     # 如果黑色已经落子，则轮到白色方落子
                     # 如果白色可以落子
-                    whether_white = canvas.check_white()
+                    whiteHasValidMove = canvas.check_white()
 
-                    if whether_white:
-
+                    if whiteHasValidMove:
                         # Human VS Human
                         if playerType == 0:
                             quitFlag = False
-                            white_clicked = False
+                            whiteClicked = False
                             while 1:
-                                if white_clicked:
+                                if whiteClicked:
                                     break
 
                                 p4 = win.getMouse()
@@ -204,7 +230,7 @@ if __name__ == '__main__':
                                             canvas.piece_num[1] += 1
                                             canvas.total_num += 1
                                             refresh()
-                                            white_clicked = True
+                                            whiteClicked = True
 
                             if quitFlag:
                                 break
@@ -236,9 +262,9 @@ if __name__ == '__main__':
                                 a = hardModeAI.calculate.intArray(64)
                                 for i in xrange(8):
                                     for j in xrange(8):
-                                        if canvas.board[i][j].color == 1: # white
+                                        if canvas.board[i][j].color == 1:  # white
                                             a[i * 8 + j] = 0
-                                        elif canvas.board[i][j].color == 2: # black
+                                        elif canvas.board[i][j].color == 2:  # black
                                             a[i * 8 + j] = 1
                                         else:
                                             a[i * 8 + j] = -1
@@ -252,18 +278,13 @@ if __name__ == '__main__':
                             canvas.piece_num[1] += 1
                             canvas.total_num += 1
                             refresh()
+                    else:  # white doesn't have valid move
+                        t_skip_white.draw(win)
+                        sleep(1)
+                        t_skip_white.undraw()
 
                     white_turn.undraw()
                     black_turn.draw(win)
-
-                    if canvas.check_black():
-                        # 更新undoList
-                        for i in xrange(8):
-                            for j in xrange(8):
-                                undoList[i][j] = lastMove[i][j]
-                        for i in xrange(8):
-                            for j in xrange(8):
-                                lastMove[i][j] = canvas.board[i][j].color
 
                 # check whether someone wins
                 who_win = 0
@@ -271,10 +292,11 @@ if __name__ == '__main__':
 
                 if canvas.piece_num[1] + canvas.piece_num[2] < 64:
                     # 白色和黑色是否可以落子
-                    whether_white = canvas.check_white()
-                    whether_black = canvas.check_black()
+                    whiteHasValidMove = canvas.check_white()
+                    blackHasValidMove = canvas.check_black()
 
-                    if (not whether_white) and (not whether_black):
+                    if (not whiteHasValidMove) and (not blackHasValidMove):
+                        b_undo.clear()
                         b_re.redraw()  # 显示restart按钮
                         if canvas.piece_num[1] > canvas.piece_num[2]:
                             who_win = 1  # 白色获胜
@@ -290,6 +312,7 @@ if __name__ == '__main__':
                 if canvas.piece_num[1] == 0:
                     # 无白子， 黑色获胜
                     who_win = 2
+                    b_undo.clear()
                     b_re.redraw()
                     black_win.draw(win)
                     game_over = True
@@ -297,12 +320,14 @@ if __name__ == '__main__':
                 elif canvas.piece_num[2] == 0:
                     # 无黑子， 白色获胜
                     who_win = 1
+                    b_undo.clear()
                     b_re.redraw()
                     white_win.draw(win)
                     game_over = True
 
                 elif canvas.piece_num[1] + canvas.piece_num[2] == 64:
                     # 棋盘下满
+                    b_undo.clear()
                     b_re.redraw()
                     if canvas.piece_num[1] > canvas.piece_num[2]:
                         who_win = 1
@@ -321,15 +346,15 @@ if __name__ == '__main__':
 
                         if b_re.clicked(p3):
                             for i in canvas.board:
-                                 for j in i:
+                                for j in i:
                                     j.clear()
                             try:
                                 white_turn.undraw()
-                            except:
+                            except Exception:
                                 pass
                             try:
                                 black_turn.undraw()
-                            except:
+                            except Exception:
                                 pass
 
                             black_turn.draw(win)
@@ -337,6 +362,7 @@ if __name__ == '__main__':
                             refresh()
 
                             b_re.clear()
+                            b_undo.redraw()
 
                             if who_win == 0:
                                 tie.undraw()
@@ -348,14 +374,14 @@ if __name__ == '__main__':
                             logging.info('A new game starts!')
 
                             undoList = [[0, 0, 0, 0, 0, 0, 0, 0],
-                                         [0, 0, 0, 0, 0, 0, 0, 0],
-                                         [0, 0, 0, 0, 0, 0, 0, 0],
-                                         [0, 0, 0, 1, 2, 0, 0, 0],
-                                         [0, 0, 0, 2, 1, 0, 0, 0],
-                                         [0, 0, 0, 0, 0, 0, 0, 0],
-                                         [0, 0, 0, 0, 0, 0, 0, 0],
-                                         [0, 0, 0, 0, 0, 0, 0, 0]
-                                         ]
+                                        [0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 1, 2, 0, 0, 0],
+                                        [0, 0, 0, 2, 1, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0]
+                                        ]
                             break
 
                         elif b_qt.clicked(p3):
